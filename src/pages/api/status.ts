b17c2ff1b612764da5591b41d322dc875a988c41
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2021-02-28 01:36:31 +0300

    feat: add status endpoint

---
+++ src/pages/api/status.ts
@@ -0,0 +1,52 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+const fileLabel = 'api/status';
+
+/**
+ * Status endpoint - Prints the "status" of the deployed instance.
+ *
+ * Prints useful information regarding the deployment.
+ * Meant to be used for debugging purposes.
+ * Can also be used as "ping endpoint" to make sure the app is online.
+ *
+ * @param req
+ * @param res
+ * @method GET
+ */
+export const status = (req: NextApiRequest, res: NextApiResponse): void => {
+  try {
+    res.json({
+      appStage: process.env.NEXT_PUBLIC_APP_STAGE,
+      appName: process.env.NEXT_PUBLIC_APP_NAME,
+      appRelease: process.env.NEXT_PUBLIC_APP_VERSION_RELEASE,
+      appBuildTime: process.env.NEXT_PUBLIC_APP_BUILD_TIME,
+      appBuildTimeISO: new Date(process.env.NEXT_PUBLIC_APP_BUILD_TIME || null).toISOString(),
+      appBuildTimestamp: process.env.NEXT_PUBLIC_APP_BUILD_TIMESTAMP,
+      appBuildId: process.env.NEXT_PUBLIC_APP_BUILD_ID,
+      nodejs: process.version,
+      nodejsAWS: process.env.AWS_EXECUTION_ENV,
+      regionVERCEL: process.env.VERCEL_REGION,
+      regionAWS: process.env.AWS_REGION,
+      timezone: process.env.TZ,
+      memory: process.env.AWS_LAMBDA_FUNCTION_MEMORY_SIZE,
+      environment: process.env.NODE_ENV,
+      preset: process.env.NEXT_PUBLIC_NRN_PRESET,
+      customerRef: process.env.NEXT_PUBLIC_CUSTOMER_REF,
+      GIT_COMMIT_SHA: process.env.GIT_COMMIT_SHA,
+      GIT_COMMIT_REF: process.env.GIT_COMMIT_REF,
+      GIT_COMMIT_TAGS: process.env.GIT_COMMIT_TAGS,
+    });
+  } catch (e) {
+    res.json({
+      error: true,
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      message:
+        process.env.NEXT_PUBLIC_APP_STAGE === 'production'
+          ? undefined
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          : e.message,
+    });
+  }
+};
+
+export default status;

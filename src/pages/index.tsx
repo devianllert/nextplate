bf74bb95cc5fb4846e2fb142ccb898b2fa59fc3d
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2021-02-27 18:49:56 +0300

    feat: add eslint

---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <Head>
+      <title>Create Next App</title>
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+  );
+}

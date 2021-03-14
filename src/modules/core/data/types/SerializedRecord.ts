0f1b2046251d46018c7287769a9d991c2f3f5dc6
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2021-03-15 00:48:01 +0300

    feat: add useful typings

---
+++ src/modules/core/data/types/SerializedRecord.ts
@@ -0,0 +1,10 @@
+/**
+ * Represents a record that is serialized (url-friendly)
+ *
+ * Meant to contain the minimal identifiable properties to be able to restore the record, eventually
+ *
+ * Used when transmitting records over the network (internet), such as:
+ *  - Encoded within a URL string
+ *  - Stored on a cache system (localstorage, cookie, etc.)
+ */
+export type SerializedRecord = string;

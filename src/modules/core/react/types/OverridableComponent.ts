3b4dd8e6a7f00a83222906546ae6bd816718ca52
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2021-03-26 21:07:00 +0300

    feat: add overridable component type

--- src/modules/core/react/types/OverridableComponent.ts
+++ src/modules/core/react/types/OverridableComponent.ts
@@ -1,16 +1,63 @@
 /* eslint-disable @typescript-eslint/ban-types */
-import { ElementType, ComponentPropsWithRef } from 'react';
 
-export type OverrideProps<C extends ElementType, P = {}> = P & Omit<ComponentPropsWithRef<C>, keyof P>;
+/**
+ * Remove properties `K` from `T`.
+ * Distributive for union types.
+ *
+ * @internal
+ */
+export type DistributiveOmit<T, K extends keyof any> = T extends any ? Omit<T, K> : never;
 
-export interface OverridableComponent<P = {}> {
-  <C extends ElementType>(
+/**
+ * A component whose root component can be controlled via a `component` prop.
+ *
+ * Adjusts valid props based on the type of `component`.
+ */
+export interface OverridableComponent<M extends OverridableTypeMap> {
+  <C extends React.ElementType>(
     props: {
       /**
        * The component used for the root node.
        * Either a string to use a HTML element or a component.
        */
-      component?: C;
-    } & OverrideProps<C, P>,
+      component: C;
+    } & OverrideProps<M, C>,
   ): JSX.Element;
+  <C extends React.ElementType>(props: DefaultComponentProps<M, C>): JSX.Element;
+}
+
+/**
+ * Props of the component if `component={Component}` is used.
+ */
+export type OverrideProps<
+  M extends OverridableTypeMap,
+  C extends React.ElementType,
+> = (
+  & BaseProps<M>
+  & DistributiveOmit<React.ComponentPropsWithRef<C>, keyof BaseProps<M>>
+);
+
+/**
+ * Props if `component={Component}` is NOT used.
+ */
+// prettier-ignore
+export type DefaultComponentProps<M extends OverridableTypeMap, C extends React.ElementType> =
+  & BaseProps<M> & {
+    /**
+        * The component used for the root node.
+        * Either a string to use a HTML element or a component.
+        */
+    component?: C;
+  }
+  & DistributiveOmit<React.ComponentPropsWithRef<M['defaultComponent']>, keyof BaseProps<M>>;
+
+/**
+ * Props defined on the component (+ common material-ui props).
+ */
+// prettier-ignore
+export type BaseProps<M extends OverridableTypeMap> = M['props'];
+
+export interface OverridableTypeMap {
+  props: {};
+  defaultComponent: React.ElementType;
 }

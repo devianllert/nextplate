bd6b8800c13b90b1b84789a439d56c5ff038afa5
Ruslan Povolockii <33530303+devianllert@users.noreply.github.com>
2021-03-26 21:11:29 +0300

    feat: add useHasMounted hook

---
+++ src/common/hooks/useHasMounted.tsx
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+
+/**
+ * Utility hook to properly handle expected differences between server and browser rendering.
+ * Helps to avoid "Text content did not match" warnings, during React rehydration.
+ *
+ * Similar to "DisplayOnBrowserMount" component, but as a hook.
+ *
+ * @see https://joshwcomeau.com/react/the-perils-of-rehydration/#abstractions Strongly inspired from useHasMounted
+ */
+const useHasMounted = (): boolean => {
+  const [hasMounted, setHasMounted] = useState<boolean>(false);
+
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
+  return hasMounted;
+};
+
+export default useHasMounted;

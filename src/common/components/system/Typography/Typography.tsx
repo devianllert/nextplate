d71cef53e51058f8496b074f7f5aa223415bf53f
Ruslan Povolockii <devianllert@gmail.com>
2021-05-25 19:02:36 +0300

    chore: fix code style

--- src/common/components/system/Typography/Typography.tsx
+++ src/common/components/system/Typography/Typography.tsx
@@ -119,5 +119,3 @@ export const Typography: OverridableComponent<TypographyTypeMap> = forwardRef(fu
     </S.Typo>
   );
 });
-
-export default Typography;
--- src/common/components/system/Typography/index.ts
+++ src/common/components/system/Typography/index.ts
@@ -1,4 +1,1 @@
-import Typography from './Typography';
-
 export * from './Typography';
-export default Typography;

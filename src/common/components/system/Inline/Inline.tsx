eac01eec03dbddce766648dc6639fe037409067f
Ruslan Povolockii <devianllert@gmail.com>
2021-06-08 17:01:17 +0300

    feat: add tests for inline component

--- src/common/components/system/Inline/Inline.tsx
+++ src/common/components/system/Inline/Inline.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 import flattenChildren from 'react-keyed-flatten-children';
 
+import { OverridableComponent } from '@/modules/core/react/types/OverridableComponent';
+
 import * as S from './styled';
 
 export interface InlineProps {
@@ -18,26 +20,35 @@ export interface InlineProps {
   alignY?: React.CSSProperties['alignItems'];
 }
 
+export interface InlineTypeMap<P = {}, D extends React.ElementType = 'div'> {
+  props: P & InlineProps;
+  defaultComponent: D;
+}
+
 /**
- * If you’d like to render a set of components in a row with equal spacing around them,
- * wrapping onto multiple lines when necessary, Braid provides an Inline component:
+ * The Inline component render a set of components in a row with equal spacing around them,
+ * wrapping onto multiple lines when necessary.
  */
-export const Inline = (props: InlineProps): JSX.Element => {
+export const Inline: OverridableComponent<InlineTypeMap> = React.forwardRef((props, ref) => {
   const {
+    component = 'div',
     children,
     space = 8,
     alignY = 'inherit',
   } = props;
 
+  const isList = component === 'ol' || component === 'ul';
+  const inlineItemComponent = isList ? 'li' : 'div';
+
   return (
-    <S.InlineRootAligner space={space}>
-      <S.InlineRoot marginTop={space} marginLeft={space} alignY={alignY}>
+    <S.InlineRootAligner space={space} ref={ref}>
+      <S.InlineRoot as={component} marginTop={space} marginLeft={space} alignY={alignY}>
         {React.Children.map(flattenChildren(children), (child) => (child ? (
-          <S.InlineBox space={space}>{child}</S.InlineBox>
+          <S.InlineBox space={space} as={inlineItemComponent}>{child}</S.InlineBox>
         ) : null))}
       </S.InlineRoot>
     </S.InlineRootAligner>
   );
-};
+});
 
 export default Inline;
--- src/common/components/system/Inline/tests/Inline.test.tsx
+++ src/common/components/system/Inline/tests/Inline.test.tsx
@@ -2,17 +2,59 @@
  * @jest-environment jsdom
  */
 
-import React, { ReactChild } from 'react';
-import { render, screen } from '@testing-library/react';
+import React from 'react';
 
-import { Inline } from '../Inline';
+import { renderWithProviders } from '@/modules/tests/renderWithProviders';
+
+import { Typography } from '../../Typography';
+
+import { Inline } from '..';
 
 describe('<Inline />', () => {
-  const children: ReactChild = 'Inline';
+  it('should not render a list by default', () => {
+    const { queryAllByRole } = renderWithProviders(
+        <Inline space={8}>
+          <Typography>1</Typography>
+          <Typography>2</Typography>
+          <Typography>3</Typography>
+        </Inline>
+    );
 
-  it('should render a children', () => {
-    render(<Inline>{children}</Inline>);
+    expect(queryAllByRole('list').length).toBe(0);
+    expect(queryAllByRole('listItem').length).toBe(0);
+  });
 
-    expect(screen.getByText(children)).toBeDefined();
+  it('should render a valid unordered list when "component" is "ul"', () => {
+    const { getByRole } = renderWithProviders(
+      <Inline component="ul" space={8}>
+        <Typography>1</Typography>
+        <Typography>2</Typography>
+        <Typography>3</Typography>
+      </Inline>
+    );
+
+    const list = getByRole('list');
+    expect(list.nodeName).toBe('UL');
+
+    expect(
+      Array.from(list.childNodes).map((childNode) => childNode.nodeName),
+    ).toEqual(['LI', 'LI', 'LI']);
+  });
+
+  it('should render a valid ordered list when "component" is "ol"', () => {
+    const { getByRole } = renderWithProviders(
+      <Inline component="ol" space={8}>
+        <Typography>1</Typography>
+        <Typography>2</Typography>
+        <Typography>3</Typography>
+      </Inline>
+    );
+
+    const list = getByRole('list');
+    expect(list.nodeName).toBe('OL');
+
+    expect(
+      Array.from(list.childNodes).map((childNode) => childNode.nodeName),
+    ).toEqual(['LI', 'LI', 'LI']);
   });
 });

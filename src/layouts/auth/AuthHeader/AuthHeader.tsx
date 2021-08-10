60696a07576f270b44609329b593e6870e33628b
Ruslan Povolockii <devianllert@gmail.com>
2021-08-10 21:05:41 +0300

    chore: add main link to auth page

--- src/layouts/auth/AuthHeader/AuthHeader.tsx
+++ src/layouts/auth/AuthHeader/AuthHeader.tsx
@@ -1,4 +1,7 @@
 import * as React from 'react';
+import Link from 'next/link';
+
+import { APP_TITLE } from '@/modules/core/meta/meta';
 
 import { Typography } from '@/common/components/system/Typography';
 
@@ -9,7 +12,9 @@ export type AuthHeaderProps = unknown;
 export const AuthHeader = (): JSX.Element => {
   return (
     <S.AuthHeaderRoot>
-      <Typography variant="h6" component="span">dvnllrt-app</Typography>
+      <Link href="/" passHref>
+        <Typography variant="h6" component="a">{APP_TITLE}-app</Typography>
+      </Link>
     </S.AuthHeaderRoot>
   );
 };

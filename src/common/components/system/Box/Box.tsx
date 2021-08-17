172ec30e1fe232cfd27719f45675314a9ad733f4
Ruslan Povolockii <devianllert@gmail.com>
2021-08-17 21:02:24 +0300

    feat: make the Box component overridable

--- src/common/components/system/Box/Box.tsx
+++ src/common/components/system/Box/Box.tsx
@@ -1,4 +1,7 @@
+/* eslint-disable prefer-arrow-callback */
 import * as React from 'react';
+
+import { OverridableComponent } from '@/modules/core/react/types/OverridableComponent';
 
 import * as S from './styled';
 
@@ -9,13 +12,18 @@ export interface BoxProps extends S.BoxType {
   children: React.ReactNode;
 }
 
-export const Box = (props: BoxProps): JSX.Element => {
+export interface BoxTypeMap<P = {}, D extends React.ElementType = 'div'> {
+  props: P & BoxProps;
+  defaultComponent: D
+}
+
+export const Box: OverridableComponent<BoxTypeMap> = React.forwardRef(function Box(props, ref) {
   const {
     children,
     ...other
   } = props;
 
   return (
-    <S.BoxRoot {...other}>{children}</S.BoxRoot>
+    <S.BoxRoot {...other} ref={ref}>{children}</S.BoxRoot>
   );
-};
+});

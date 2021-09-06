d3534ae2ed752686078df3fd27c9b190e0f5b9ba
Ruslan Povolockii <devianllert@gmail.com>
2021-09-06 15:16:08 +0300

    chore: fix default import

--- src/common/design/tokens/zIndex.ts
+++ src/common/design/tokens/zIndex.ts
@@ -1,11 +1,9 @@
 // We need to centralize the zIndex definitions as they work
 // like global values in the browser.
-const zIndex = {
+export const zIndex = {
   appBar: 1100,
   drawer: 1200,
   modal: 1300,
   snackbar: 1400,
   tooltip: 1500,
 };
-
-export default zIndex;

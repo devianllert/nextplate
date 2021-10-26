7c194e9fd13c1ae51a76215c3d5f668002451731
Ruslan Povolockii <devianllert@gmail.com>
2021-10-27 02:58:35 +0300

    fix: fix jest error messages

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,12 @@
 // "package.json:jest" config cannot be used alongside this config, all Jest config must be centralised in this file - See https://github.com/facebook/jest/issues/10123#issuecomment-638796267
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  globals: {
-    // XXX we must specify a custom tsconfig for tests because we need the typescript transform
-    //  to transform jsx into js rather than leaving it jsx such as the next build requires.  you
-    //  can see this setting in tsconfig.jest.json -> "jsx": "react"
-    //  See https://github.com/vercel/next.js/issues/8663
-    'ts-jest': {
-      tsconfig: 'tsconfig.jest.json',
-    },
+  verbose: true,
+  transform: {
+    // Use babel-jest to transpile tests with the next/babel preset
+    // https://jestjs.io/docs/configuration#transform-objectstring-pathtotransformer--pathtotransformer-object
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
   /**
    * Map our module path aliases, so that Jest can understand modules loaded using "@/common" and load the proper file.
    * Required, or Jest will fail to import dependencies from tests.
@@ -38,8 +33,7 @@ module.exports = {
   runner: 'groups', // Allow to use jest-runner-groups - See https://github.com/eugene-manuilov/jest-runner-groups#update-jest-config
   setupFilesAfterEnv: [
     'jest-extended', // Extends native "expect" abilities - See https://github.com/jest-community/jest-extended
-    'jest-expect-message', // Allows to add additional message when test fails - See https://github.com/mattphillips/jest-expect-message
-    './jest.setup.js',
-    './jest.extends.ts',
+    '<rootDir>/jest.setup.js',
+    '<rootDir>/jest.extends.ts',
   ],
 };
--- jest.extends.ts
+++ jest.extends.ts
@@ -4,7 +4,7 @@
 // XXX All expect.extend() utilities loaded here will be available for all tests, they also might need to be declared in jest.d.ts
 import { toMatchOneOf, toMatchShapeOf } from 'jest-to-match-shape-of'; // See https://www.npmjs.com/package/jest-to-match-shape-of
 import { matchers } from '@emotion/jest';
-import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 
 // Extend Jest "expect" function
 expect.extend({

4679f697bd0a37e976a19ff8366ade28a67fd515
Ruslan Povolockii <devianllert@gmail.com>
2022-04-07 20:58:42 +0300

    feat: change default font-family

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -32,8 +32,7 @@ class AppDocument extends Document {
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet" />
-
+          <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700&display=swap" rel="stylesheet" />
           <style
             type="text/css"
             dangerouslySetInnerHTML={{ __html: mediaStyles }}

3793ebfb580a96feeb0f4e1516170e7a5380cebf
Ruslan Povolockii <devianllert@gmail.com>
2022-08-28 22:05:36 +0300

    feat: add cors for api routes

--- next.config.js
+++ next.config.js
@@ -207,7 +207,7 @@ module.exports = withSentryConfig(
             // See https://scotthelme.co.uk/a-new-security-header-referrer-policy/
             {
               key: 'Referrer-Policy',
-              value: 'origin-when-cross-origin',
+              value: 'strict-origin-when-cross-origin',
             },
 
             /* This header controls DNS prefetching,
@@ -232,6 +232,27 @@ module.exports = withSentryConfig(
             {
               key: 'Content-Security-Policy',
               value: ContentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+            },
+          ],
+        },
+        {
+          source: '/api/:path*',
+          headers: [
+            {
+              key: 'Access-Control-Allow-Credentials',
+              value: 'true',
+            },
+            {
+              key: 'Access-Control-Allow-Origin',
+              value: process.env.NEXT_PUBLIC_APP_URL,
+            },
+            {
+              key: 'Access-Control-Allow-Methods',
+              value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+            },
+            {
+              key: 'Access-Control-Allow-Headers',
+              value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
             },
           ],
         },

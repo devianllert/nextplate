aa8250fb55976459994790c65bc535abf940ea91
Ruslan Povolockii <devianllert@gmail.com>
2022-10-07 11:04:20 +0300

    fix(weather): fix hydration error

--- src/entities/weather/components/weather-hourly-list.tsx
+++ src/entities/weather/components/weather-hourly-list.tsx
@@ -8,6 +8,7 @@ import { Container } from '@/shared/components/system/container';
 import { ICONS_MAP } from '../constants/icons-map';
 import { formatHourlyTime } from '../lib/format-hourly-weather';
 import { WTTRWeatherHourlyCondition } from '../types/weather.interface';
+import { DisplayOnBrowserMount } from '@/shared/components/rehydration/display-on-browser-mount';
 
 export interface WeatherHourlyListProps {
   hourlyWeather: WTTRWeatherHourlyCondition[];
@@ -25,22 +26,33 @@ export const WeatherHourlyList = (props: WeatherHourlyListProps): JSX.Element =>
       overflowX="auto"
       px={[0, null]}
     >
-      {hourlyWeather.map((item, index) => (
-        <Box
-          key={item.time}
-          display="flex"
-          flexDirection="column"
-          alignItems="center"
-          minWidth={[140, null, 160]}
-          px={[2, null, 4]}
-          py={2}
-        >
-          <Text.Heading variant="h6" component="span">{index === 0 ? 'Now' : formatHourlyTime(item.time)}</Text.Heading>
-          <Image width="64" height="64" src={`/static/images/weather/wi-${ICONS_MAP[item.weatherCode]}.svg`} alt={item.weatherCode} />
+      <DisplayOnBrowserMount>
+        {hourlyWeather.map((item, index) => (
+          <Box
+            key={item.time}
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            minWidth={[140, null, 160]}
+            px={[2, null, 4]}
+            py={2}
+          >
+            <Text.Heading variant="h6" component="span">
+              {index === 0 ? 'Now' : formatHourlyTime(item.time)}
+            </Text.Heading>
+            <Image
+              width="64"
+              height="64"
+              src={`/static/images/weather/wi-${ICONS_MAP[item.weatherCode]}.svg`}
+              alt={item.weatherCode}
+            />
 
-          <Text.Heading variant="h6" component="span">{item.tempC} °</Text.Heading>
-        </Box>
-      ))}
+            <Text.Heading variant="h6" component="span">
+              {item.tempC} °
+            </Text.Heading>
+          </Box>
+        ))}
+      </DisplayOnBrowserMount>
     </Box>
   );
 };

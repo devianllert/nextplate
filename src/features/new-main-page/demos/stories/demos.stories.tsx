e288d65a2b266c0ab2b3b41f3a077e6427f20fed
sergey <33asasergey33@gmail.com>
2023-01-04 20:48:05 +0300

    fix: demo stories

--- src/features/new-main-page/demos/stories/demos.stories.tsx
+++ src/features/new-main-page/demos/stories/demos.stories.tsx
@@ -1,17 +1,15 @@
 import * as React from 'react';
 import { Story, Meta } from '@storybook/react';
 
-import { Demos, DemosProps } from '../demos';
+import { Demos } from '../demos';
 
 export default {
   title: 'Demos',
   component: Demos,
 } as Meta;
 
-const Template: Story<DemosProps> = (args) => <Demos {...args} />;
+const Template: Story = (args) => <Demos {...args} />;
 
 export const Basic = Template.bind({});
 
-Basic.args = {
-  children: 'Demos',
-};
+Basic.args = {};

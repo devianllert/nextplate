75f2541ad3a86100661003516edbf4da2a62fc31
devianllert <devianllert@gmail.com>
2023-03-28 10:52:38 +0300

    feat: improve paths in middleware matcher

--- src/middleware.page.ts
+++ src/middleware.page.ts
@@ -12,6 +12,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/((?!api|static/images|_next/static|_next/image|site.webmanifest|robots.txt).*)',
   ],
 };
